08863cd1f6439a7ed02cfc867f7d9b4ee063b2e8
lingsb <3422284835@qq.com>
2024-03-25 18:59:34 +0800

    管理员完成订单功能完善

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -106,4 +106,15 @@ public class OrderController {
         orderService.delivery(id);
         return Result.success();
     }
+    /**
+     * 完成订单
+     *
+     * @return
+     */
+    @PutMapping("/complete/{id}")
+    @ApiOperation("完成订单")
+    public Result complete(@PathVariable("id") Long id) {
+        orderService.complete(id);
+        return Result.success();
+    }
 }
