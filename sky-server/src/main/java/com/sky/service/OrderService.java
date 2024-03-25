d7074b8ab3185d79fd736c1b1a3e72be79e3ca78
lingsb <3422284835@qq.com>
2024-03-25 18:32:51 +0800

    订单搜索功能完善

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -50,4 +50,11 @@ public interface OrderService {
      * @param id
      */
     void repetition(Long id);
+
+    /**
+     * 条件搜索订单
+     * @param ordersPageQueryDTO
+     * @return
+     */
+    PageResult conditionSearch(OrdersPageQueryDTO ordersPageQueryDTO);
 }
