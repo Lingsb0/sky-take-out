10f3c07644473bf10b53b95902387cc314170989
lingsb <3422284835@qq.com>
2024-03-26 19:18:52 +0800

    订单功能完善

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -63,4 +63,9 @@ public interface OrderMapper {
      * @param map
      */
     Double sumByMap(Map map);
+    /**
+     *根据动态条件统计订单数量
+     * @param map
+     */
+    Integer countByMap(Map map);
 }
